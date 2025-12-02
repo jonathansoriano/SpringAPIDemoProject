66d3c774c6a485a86f2a003bb115158c9a5636ee
Jon Sanjuan <jonathan.s.sanjuan@outlook.com>
2025-12-02 16:22:07 -0500

    Create a CODEOWNERS file inside my new .github folder. Also added some input validation in Student Service file.

--- src/main/java/com/jonathansoriano/springapidemo/service/StudentService.java
+++ src/main/java/com/jonathansoriano/springapidemo/service/StudentService.java
@@ -52,6 +52,23 @@ public class StudentService {
             throw new CreationFailure("University with ID " + request.getUniversityId() + " does not exist");
         }
 
+        //Verify that capitalization is correct prior to insertion otherwise correct it
+        if (!isStringInputFormattedCorrectly(request.getFirstName())){
+
+            request.setFirstName(properNounStringFormatter(request.getFirstName()));
+            System.out.println("This is the updated firstName: " + request.getFirstName());
+        }
+        if (!isStringInputFormattedCorrectly(request.getLastName())){
+
+            request.setLastName(properNounStringFormatter(request.getLastName()));
+            System.out.println("This is the updated lastName: " + request.getLastName());
+        }
+        if (!isStringInputFormattedCorrectly(request.getResidentCity())){
+
+            request.setResidentCity(properNounStringFormatter(request.getResidentCity()));
+            System.out.println("This is the updated Resident City: " + request.getResidentCity());
+        }
+
         int response = repository.insertNewStudent(request);
         if (response != 1) {
             throw new CreationFailure("Student Creation Failed!");
@@ -73,7 +90,7 @@ boolean universityExists(Long universityId)
          */
         Integer count = jdbcTemplate.queryForObject(sql, params, Integer.class); //NamedJDBCTemplate can throw exceptions... like EmptyResultDataAccessException if there are no rows. IncorrectResultSizeDataAccessException is thrown if many rows are found.
 
-        return count != null && count > 0; //Check if value in the DB is null or if the university or many instances of the university exists in the DB
+        return count != null && count > 0; //Checks if value in the DB is NOT null or if the university or many instances of the university exists in the DB
     } catch (EmptyResultDataAccessException e) { //This exception is thrown if no rows are found
         return false;
     } catch (DataAccessException e) { //This is thrown for any other database issues.
@@ -105,7 +122,13 @@ boolean universityExists(Long universityId)
                 .grade(student.getGrade())
                 .build();
     }
-
+    /**
+     * Checks if any mandatory field within the provided StudentRequest object
+     * is either null or an empty string.
+     * @param request The StudentRequest object containing potential null or empty data.
+     * @return True if any required field (first name, last name, city, state, grade,
+     * university ID, or DOB) is null or empty; false otherwise.
+     */
     private boolean isInputNullOrEmpty(StudentRequest request)
     {
         return request.getFirstName() == null || request.getFirstName().matches("^*$") ||
@@ -114,5 +137,44 @@ boolean universityExists(Long universityId)
                 request.getResidentState() == null || request.getResidentState().matches("^*$") ||
                 request.getGrade() == null || request.getGrade().matches("^*$") ||
                 request.getUniversityId() == null || request.getDob() == null;
+    }
+
+
+    boolean isStringInputFormattedCorrectly(String input){
+        return input.matches("b[A-Z][a-z]*b");
+    }
+
+    /**
+     * Standardizes proper nouns by capitalizing the first letter and lowercasing the rest.
+     * @param input a string that needs corrected
+     * @return a string
+     */
+    String properNounStringFormatter (String input){
+        //Use to increment the placement of the new inserted char in a char array
+        int counter = 0;
+
+        //Turn firstname into a char array so we can parse through it
+        char [] oldFirstName = input.toCharArray();
+
+        //Create a new char array with updated chars (with the correct name format)
+        char [] newFirstName = new char[input.length()];
+
+        //Combine elements in the newFirstName array into a single StringBuilder
+        StringBuilder resultingFirstName = new StringBuilder();
+
+        for (char letter : oldFirstName)
+        {
+            if (letter != oldFirstName[0])
+            {
+                newFirstName[1 + counter] = Character.toLowerCase(letter);
+                counter++;
+            }
+            else
+            {
+                newFirstName[0] =Character.toUpperCase(letter);
+            }
+        }
+
+        return resultingFirstName.append(newFirstName).toString();
     }
 }
